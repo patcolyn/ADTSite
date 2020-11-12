b30c18188c1d639533da8490305071a045c38055
Pat Colyn - 656815 <656815@student.cavc.ac.uk>
2020-11-12 15:30:49 +0000

    Add template js library

---
+++ js/template.js
@@ -0,0 +1,29 @@
+//https://www.freecodecamp.org/news/reusable-html-components-how-to-reuse-a-header-and-footer-on-a-website/
+
+'<!-- Navbar -->
+<nav class="navbar sticky-top navbar-expand-lg navbar-expand-md navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">
+  	<img src="img/logo.svg" width="50" height="36" alt="Logo">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Navbar Links -->
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav mx-auto">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Menu</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Gallery</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About Us</a>
+      </li>
+    </ul>
+  </div>
+</nav>'
